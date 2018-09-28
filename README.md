0ce0bc6a6fcc77728851bd1429609d99e69587de
Thomas Vladeck <thomasvladeck@Thomass-Air.home>
2018-09-27 21:37:43 -0400

    initial solution

---
+++ README.md
@@ -0,0 +1,36 @@
+
+This prolog program solves the classic wedding table problem, where you have people that you want to sit next at the same table, and people that you want to make sure sit at different tables, given a certain number of tables each with a certain number of seats.
+
+The entry point is `arrangment/4`, whose arguments are:
+  - The list of lists of who is sitting at which table (this will be left specified as logic variables)
+  - The list of number of seats at each table, e.g. [3,3] for two tables each with three seats
+  - A list of pairwise pairings, e.g. [[Trevor, Tom], [Ellen, Sarah], [Trevor, Gabby]] which constrains that Trevor, Tom, and Gabby sit at the same table, and Ellen and Sarah sit at the same table
+  - A lis of pairwise exclusions, e.g. [[Ellen, Tom], [Sarah, Trevor]], which constrains that Ellen & Tom will sit at different tables and Sarah and Trevor will sit at different tables
+
+
+The way to run the program is to call `arrangement/4` with like so
+
+```
+?- consult('table.pl').
+?- arrangment([X,Y], [3,3], [[a,b], [c,d]], [[a,c]]).
+X = [a, b, a],
+Y = [c, d, c]
+% variables may be duplicated within the same table if there is space
+```
+
+This trivial example finds two tables X and Y such that:
+
+- They both have 3 seats
+- `a` and `b` sit at the same table
+- `c` and `d` sit at the same table
+- `a` and `c` sit at different tables
+
+Impossible arrangements will fail:
+
+```
+?- consult('table.pl').
+?- arrangment([X,Y], [3,3], [[a,b], [c,d]], [[a,b]]).
+false.
+```
+
+
---
+++ table.pl
@@ -0,0 +1,76 @@
+
+
+strict_member(_, [_]) :- fail.
+
+strict_member(X, [Head|Tail]) :-
+    X = Head;
+    strict_member(X, Tail).
+
+
+
+not_member(_, []) :- !.
+
+not_member(X, [Head|Tail]) :-
+     X \= Head,
+    not_member(X, Tail).
+
+at_this_table(Person1, Person2, ThisTable) :-
+    member(Person1, ThisTable),
+    member(Person2, ThisTable).
+
+at_same_table(Person1, Person2, AllTables) :-
+    member(ThisTable, AllTables),
+    at_this_table(Person1, Person2, ThisTable).
+
+pair_at_same_table(Pair, AllTables) :-
+    nth1(1, Pair, Person1),
+    nth1(2, Pair, Person2),
+    at_same_table(Person1, Person2, AllTables).
+
+at_different_tables(Person1, Person2, AllTables) :-
+    member(Table1, AllTables),
+    member(Table2, AllTables),
+    member(Person1, Table1),
+    member(Person2, Table2),
+    intersection(Table1, Table2, []).
+
+
+pair_at_different_tables([], _) :- !.
+
+pair_at_different_tables(Pair, AllTables) :-
+    nth1(1, Pair, Person1),
+    nth1(2, Pair, Person2),
+    at_different_tables(Person1, Person2, AllTables).
+
+assign_length(X, Ts, Ls) :- 
+    nth1(X, Ts, T),
+    nth1(X, Ls, L),
+    length(T, L).
+
+ground_member(X, Y) :-
+    member(X, Y),
+    ground(X).
+
+ground_members_of_table(Table, GroundTable) :-
+    findall(X, ground_member(X, Table), GroundTable).
+
+not_ground_member_of_table(Person, AnotherTable) :-
+    ground_members_of_table(AnotherTable, GroundTable),
+    \+ member(Person, GroundTable).
+
+only_at_one_table(Person, AllTables) :-
+    member(Table1, AllTables),
+    member(Person, Table1),
+    subtract(AllTables, [Table1], OtherTables),
+    foreach(member(AnotherTable, OtherTables), not_ground_member_of_table(Person, AnotherTable)).
+
+
+arrangement(Tables, Lengths, Pairs, Exclusions) :-
+    length(Tables, Y),
+    length(Lengths, Y),
+    foreach(between(1, Y, X), assign_length(X, Tables, Lengths)),
+    foreach(member(Pair, Pairs), pair_at_same_table(Pair, Tables)),
+    foreach(member(Excl, Exclusions), pair_at_different_tables(Excl, Tables)),
+    flatten(Pairs, AllPeople),
+    foreach(member(Person, AllPeople), only_at_one_table(Person, Tables)).
+
